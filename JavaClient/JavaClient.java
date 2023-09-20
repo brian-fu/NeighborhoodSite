b3afb775be47262e177d93153ce20a8a25195458
Richard Yuan <yuanrichard33@gmail.com>
2023-09-19 23:01:48 -0500

    Created Java server and test client

---
+++ JavaClient/JavaClient.java
@@ -0,0 +1,24 @@
+package JavaClient;
+
+import java.net.Socket;
+
+public class JavaClient {
+
+    private static String host = "0.0.0.0";
+
+    public static void main(String[] args) {
+        try{
+            JavaClient c = new JavaClient();
+            c.setUpNetworking();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void setUpNetworking() throws Exception {
+		@SuppressWarnings("resource")
+		Socket socket = new Socket(host, 2232);
+		System.out.println("Connecting to... " + socket);
+    }
+}
